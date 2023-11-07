d6194d9ec3ff92cffa3e629967b2c896709821c5
martubecerra <2214822@ucc.edu.ar>
2023-11-07 16:15:56 -0300

    UPDATE main-Error Handling

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,63 @@
 #include "utils/deposit/DepositTree.h"
 #include "utils/map/ArticlesMap.h"
 
+// Funciones de validación
+bool validarParametros(const std::string &param, int &numStock, int &deposito) {
+    auto params = splitCSVLine(param);
+    if (params.size() == 1) {
+        try {
+            numStock = std::stoi(params[0]);
+        } catch (const std::invalid_argument &) {
+            std::cerr << "Error: El valor n no es un numero valido." << std::endl;
+            return false;
+        }
+        if (numStock < 0) {
+            std::cerr << "Error: El valor n debe ser mayor que cero." << std::endl;
+            return false;
+        }
+    } else if (params.size() == 2) {
+        try {
+            numStock = std::stoi(params[0]);
+            deposito = std::stoi(params[1]);
+        } catch (const std::invalid_argument &) {
+            std::cerr << "Error: Los valores n o deposito no son numeros validos." << std::endl;
+            return false;
+        }
+        if (numStock < 0 || deposito < 0) {
+            std::cerr << "Error: Los valores n y deposito deben ser mayores que cero." << std::endl;
+            return false;
+        }
+    } else {
+        std::cerr << "Error: Formato incorrecto." << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool validarCodigoBarras(const std::string &codigo, const std::unordered_map<std::string, Articulo> &mapaArticulos) {
+    if (mapaArticulos.find(codigo) == mapaArticulos.end()) {
+        std::cerr << "Error: El codigo de barras no existe en el mapa de articulos." << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool validarStock(const std::string &param, const std::unordered_map<std::string, Articulo> &mapaArticulos, int &deposito) {
+    auto params = splitCSVLine(param);
+    if (params.size() == 1) {
+        if (validarCodigoBarras(params[0], mapaArticulos)) {
+            return true;
+        }
+    } else if (params.size() == 2) {
+        if (validarParametros(params[1], deposito, deposito) && validarCodigoBarras(params[0], mapaArticulos)) {
+            return true;
+        }
+    }
+    std::cerr << "Error: Formato incorrecto." << std::endl;
+    return false;
+}
+
+
 std::unordered_map<std::string, std::function<void(const std::string &)>> routerDeArgumentos(
         std::unordered_map<std::string, Articulo> &mapaArticulos,
         std::vector<std::multimap<int, std::string>> &depositos,
@@ -16,158 +73,60 @@ std::unordered_map<std::string, std::function<void(const std::string &)>> router
             {"total_art_dif", [&mapaArticulos](const std::string &param) {
                 int cantidadArticulos = getTotalArticulosDiferentes(mapaArticulos);
                 std::cout << "Cantidad de articulos diferentes: " << cantidadArticulos << std::endl;
-                return;
             }},
 
             {"total_art",     [depositosTotales](const std::string &param) {
                 std::cout << "Cantidad de articulos: " << depositosTotales << std::endl;
-                return;
             }},
 
-            /*  {"min_stock", [&depositos](const std::string& param) {
-                auto params = splitCSVLine(param);
-                if (params.size() == 1) {
-                  int minStock = std::stoi(params[0]);
-                  int totalArticulosFiltrados = mostrarArticulosConStockMenorOIgualA(depositos[0], minStock);
-                  std::cout << "Cantidad de articulos totales con stock menor o igual a " << minStock << ": " << totalArticulosFiltrados << std::endl;
-                }
-                else {
-                  int minStock = std::stoi(params[0]);
-                  int deposito = std::stoi(params[1]);
-                  int totalArticulosFiltrados = mostrarArticulosConStockMenorOIgualA(depositos[deposito], minStock);
-                  std::cout << "Cantidad de articulos en el deposito #" << deposito << " con stock menor o igual a " << minStock << ": " << totalArticulosFiltrados << std::endl;
-                }
-              }},*/
 
             {"min_stock",     [&depositos](const std::string &param) {
-                auto params = splitCSVLine(param);
-                if (params.size() == 1) {
-                    int minStock;
-                    try {
-                        minStock = std::stoi(params[0]);
-                    } catch (const std::invalid_argument &) {
-                        std::cerr << "Error: El valor n no es un número válido." << std::endl;
-                        return;
+                int minStock = 0, deposito = 0;
+                if (validarParametros(param, minStock, deposito)) {
+                    if (deposito == 0) {
+                        int totalArticulosFiltrados = mostrarArticulosConStockMenorOIgualA(depositos[0], minStock);
+                        std::cout << "Cantidad de articulos totales con stock menor o igual a " << minStock << ": "
+                                  << totalArticulosFiltrados << std::endl;
+                    } else {
+                        int totalArticulosFiltrados = mostrarArticulosConStockMenorOIgualA(depositos[deposito],
+                                                                                           minStock);
+                        std::cout << "Cantidad de articulos en el deposito #" << deposito
+                                  << " con stock menor o igual a " << minStock << ": " << totalArticulosFiltrados
+                                  << std::endl;
                     }
-                    if (minStock < 0) {
-                        std::cerr << "Error: El valor n debe ser mayor que cero." << std::endl;
-                        return;
-                    }
-                    int totalArticulosFiltrados = mostrarArticulosConStockMenorOIgualA(depositos[0], minStock);
-                    std::cout << "Cantidad de artículos totales con stock menor o igual a " << minStock << ": "
-                              << totalArticulosFiltrados << std::endl;
-                } else {
-                    // Validar el número de argumentos y el valor del depósito
-                    if (params.size() != 2) {
-                        std::cerr << "Error: Formato incorrecto." << std::endl;
-                        return;
-                    }
-                    int minStock, deposito;
-                    try {
-                        minStock = std::stoi(params[0]);
-                        deposito = std::stoi(params[1]);
-                    } catch (const std::invalid_argument &) {
-                        std::cerr << "Error: Los valores n o depósito no son números válidos." << std::endl;
-                        return;
-                    }
-                    if (minStock < 0 || deposito < 0) {
-                        std::cerr << "Error: Los valores n y depósito deben ser mayores que cero." << std::endl;
-                        return;
-                    }
-                    int totalArticulosFiltrados = mostrarArticulosConStockMenorOIgualA(depositos[deposito], minStock);
-                    std::cout << "Cantidad de artículos en el depósito #" << deposito << " con stock menor o igual a "
-                              << minStock << ": " << totalArticulosFiltrados << std::endl;
                 }
             }},
-
-
-/*    {"stock", [&mapaArticulos](const std::string& param) {
-      auto params = splitCSVLine(param);
-        std::string codigoBarras = params[0];
-      if (params.size() == 1) {
-        int stockTotal = getStockTotal(mapaArticulos, codigoBarras);
-        std::cout << "Stock total del articulo '" << codigoBarras << "': " << stockTotal << std::endl;
-      }
-      else {
-        int deposito = std::stoi(params[1]);
-        int stockTotal = getStockTotal(mapaArticulos, codigoBarras, deposito);
-        std::cout << "Stock del articulo '" << codigoBarras << "' en el deposito #" << deposito << ": " << stockTotal << std::endl;
-      }
-    }},*/
 
             {"stock",         [&mapaArticulos](const std::string &param) {
-                auto params = splitCSVLine(param);
-                if (params.size() == 1) {
-                    std::string codigoBarras = params[0];
-                    if (mapaArticulos.find(codigoBarras) == mapaArticulos.end()) {
-                        std::cerr << "Error: El código de barras no existe en el mapa de artículos." << std::endl;
-                        return;
+                int deposito = 0;
+                if (validarStock(param, mapaArticulos, deposito)) {
+                    auto params = splitCSVLine(param);
+                    if (deposito == 0) {
+                        int stockTotal = getStockTotal(mapaArticulos, params[0]);
+                        std::cout << "Stock total del articulo '" << params[0] << "': " << stockTotal << std::endl;
+                    } else {
+                        int stockTotal = getStockTotal(mapaArticulos, params[0], deposito);
+                        std::cout << "Stock del articulo '" << params[0] << "' en el deposito #" << deposito << ": "
+                                  << stockTotal << std::endl;
                     }
-                    int stockTotal = getStockTotal(mapaArticulos, codigoBarras);
-                    std::cout << "Stock total del artículo '" << codigoBarras << "': " << stockTotal << std::endl;
-                } else if (params.size() == 2) {
-                    std::string codigoBarras = params[0];
-                    int deposito;
-                    try {
-                        deposito = std::stoi(params[1]);
-                    } catch (const std::invalid_argument &) {
-                        std::cerr << "Error: El valor del depósito no es un número válido." << std::endl;
-                        return;
-                    }
-                    if (deposito <= 0) {
-                        std::cerr << "Error: El valor del depósito debe ser mayor que cero." << std::endl;
-                        return;
-                    }
-                    if (mapaArticulos.find(codigoBarras) == mapaArticulos.end()) {
-                        std::cerr << "Error: El código de barras no existe en el mapa de artículos." << std::endl;
-                        return;
-                    }
-                    int stockTotal = getStockTotal(mapaArticulos, codigoBarras, deposito);
-                    std::cout << "Stock del artículo '" << codigoBarras << "' en el depósito #" << deposito << ": "
-                              << stockTotal << std::endl;
-                } else {
-                    std::cerr << "Error: Formato incorrecto para stock." << std::endl;
                 }
             }},
 
 
-
-            /* {"max_stock", [&depositos](const std::string& param) {
-               auto params = splitCSVLine(param);
-               if (params.size() == 1) {
-                 int minStock = std::stoi(params[0]);
-                 int totalArticulosFiltrados = mostrarArticulosConStockMayorOIgualA(depositos[0], minStock);
-                 std::cout << "Cantidad de articulos totales con stock menor o igual a " << minStock << ": " << totalArticulosFiltrados << std::endl;
-               }
-             }}
-           };*/
-
             {"max_stock",     [&depositos](const std::string &param) {
-                auto params = splitCSVLine(param);
-                if (params.size() == 1) {
-                    int maxStock;
-                    try {
-                        maxStock = std::stoi(params[0]);
-                    } catch (const std::invalid_argument &) {
-                        std::cerr << "Error: El valor n no es un número válido." << std::endl;
-                        return;
-                    }
-                    if (maxStock < 0) {
-                        std::cerr << "Error: El valor n debe ser mayor que cero." << std::endl;
-                        return;
-                    }
+                int maxStock = 0;
+                if (validarParametros(param, maxStock, maxStock)) {
                     int totalArticulosFiltrados = mostrarArticulosConStockMayorOIgualA(depositos[0], maxStock);
-                    std::cout << "Cantidad de artículos totales con stock mayor o igual a " << maxStock << ": "
+                    std::cout << "Cantidad de articulos totales con stock mayor o igual a " << maxStock << ": "
                               << totalArticulosFiltrados << std::endl;
-                } else {
-                    std::cerr << "Error: Formato incorrecto." << std::endl;
                 }
             }}
     };
 
-
     return acciones;
+
 }
+
 
 int main(int argc, char *argv[]) {
     /*
